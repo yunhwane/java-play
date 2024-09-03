3d81db80b05849d94467fffd8f4bc90aaacffaa0
yunhwan-dev <wjsdbsghks96@dgu.ac.kr>
2024-09-03 21:31:56 +0900

    백준 11279 최대힙

---
+++ src/ds/beak11279.java
@@ -0,0 +1,36 @@
+package ds;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class beak11279 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> o2 - o1);
+
+        while (N--> 0) {
+            st =  new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+
+            pq.add(x);
+
+            if (x == 0) {
+                if (pq.isEmpty()) {
+                    sb.append(0).append('\n');
+                } else {
+                    sb.append(pq.poll()).append('\n');
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+}
